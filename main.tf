7e51900ad4a98ae7978aeba674bb9f6083067d52
kmei428 <80740233+kmei428@users.noreply.github.com>
2025-09-26 13:37:02 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform { 
+  cloud { 
+    
+    organization = "{Organization名}" 
+
+    workspaces { 
+      name = "{workspaces名}" 
+    } 
+  } 
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+  default_tags {
+    tags = {
+      Project     = "terraform-handson"
+    }
+  }
+}
+
+resource "aws_iam_user" "lb" {
+  name = "loadbalancer"
+  path = "/system/"
+}
